1cc177cd0c9499c3a4160342fea9b71838389b99
Parallels <francois.gnz@gmail.com>
2025-01-28 18:03:11 +0100

    modified script in wordpress container to make php-fpm listen on wordpress:9000 container

--- srcs/requirements/wordpress/conf/wordpress_setup.sh
+++ srcs/requirements/wordpress/conf/wordpress_setup.sh
@@ -2,5 +2,14 @@
 
 echo "[INFO] Hello this is Wordpress setup script.."
 
-mkdir /run/php
+#Etait necessaire pour quand php ecoutait sur sa socket , peut etre plus utile maintenant.
+if [ ! -d "/run/php" ]; then
+    echo "[INFO] Creating /run/php.."
+    mkdir /run/php
+fi
+
+#Permet de remplacer les lignes commencant par "listen = XXXXXXX" par "listen = wordpress:9000". Pour que php-fpm ecoute sur son port 9000.
+sed -i 's/^listen = .*/listen = wordpress:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+
+#Lance php-fpm en foreground (pas de daemonisation).
 /usr/sbin/php-fpm7.4 -F
