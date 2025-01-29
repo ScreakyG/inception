a7d6669dfdd35c2128b08686447df6c0c8c45598
Parallels <francois.gnz@gmail.com>
2025-01-29 23:07:15 +0100

    need to install wp cli and setup wp-config.php with it

--- srcs/requirements/wordpress/conf/wordpress_setup.sh
+++ srcs/requirements/wordpress/conf/wordpress_setup.sh
@@ -11,5 +11,7 @@ fi
 #Permet de remplacer les lignes commencant par "listen = XXXXXXX" par "listen = wordpress:9000". Pour que php-fpm ecoute sur son port 9000.
 sed -i 's/^listen = .*/listen = wordpress:9000/' /etc/php/7.4/fpm/pool.d/www.conf
 
+
+
 #Lance php-fpm en foreground (pas de daemonisation).
 /usr/sbin/php-fpm7.4 -F
