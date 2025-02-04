afe56dc265a55eb6e0e97a5e2f76b18523655f88
Parallels <francois.gnz@gmail.com>
2025-02-04 23:32:20 +0100

    better script for wordpress config , added mariadb volume

--- srcs/requirements/wordpress/conf/wordpress_setup.sh
+++ srcs/requirements/wordpress/conf/wordpress_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "[INFO] Hello this is Wordpress setup script.."
-
 #Etait necessaire pour quand php ecoutait sur sa socket , peut etre plus utile maintenant.
 if [ ! -d "/run/php" ]; then
     echo "[INFO] Creating /run/php.."
@@ -12,21 +10,32 @@ fi
 sed -i 's/^listen = .*/listen = wordpress:9000/' /etc/php/7.4/fpm/pool.d/www.conf
 
 
+### CHECK IF MARIADB IS UP ###
+
+while ! mariadb -hmariadb -u$WP_USER -p$WP_PASSWORD $WP_DATABASE; #&>/dev/null;
+do
+    echo "[INFO] MariaDB unavailable, retrying.."
+    sleep 5
+done
+echo "[INFO] MariaDB available"
+
 ### WORDPRESS INSTALL PART ###
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar #Telecharge la CLI de WP
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp #Permet de simplement utiliser 'wp' au lieu de 'php wp-cli.phar' pour les commandes de la CLI
+if [ ! -f "/var/www/html/wp-config.php" ]
+then
+    echo "[INFO] Creating a new wordpress install.."
+    wp core download --path=/var/www/html --allow-root #telecharge les fichiers d'installations de WP dans mon dossier.
+    cd /var/www/html
+    wp config create --dbhost=mariadb --dbname=$WP_DATABASE --dbuser=$WP_USER --dbpass=$WP_PASSWORD --allow-root #Connection a la Database MYSQL
+    wp core install --url=localhost --title="testinception" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root #Installation de wordpress
+    wp user create $WP_USER2_NAME  $WP_USER2_MAIL --user_pass=$WP_USER2_PASS --allow-root #Creation d'un deuxieme user pour le sujet.
+else
+    echo "[INFO] Wordpress is already installed"
+fi
 
-sleep 5 #on veut etre sur que la database est cree avant que wordpress try de se connect.
-
-wp core download --path=/var/www/html --allow-root #telecharge les fichiers d'installations de WP dans mon dossier.
 cd /var/www/html
 rm -rf index.nginx-debian.html
 
-wp config create --dbhost=mariadb --dbname=$WP_DATABASE --dbuser=$WP_USER --dbpass=$WP_PASSWORD --allow-root
-wp core install --url=localhost --title="testinception" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root
-wp user create $WP_USER2_NAME  $WP_USER2_MAIL --user_pass=$WP_USER2_PASS --allow-root
 ##############################
 
 
