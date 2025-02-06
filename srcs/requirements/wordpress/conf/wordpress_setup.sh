d8e8539c4e6317abf596234dac4c8be6b32b5c32
Parallels <francois.gnz@gmail.com>
2025-02-06 01:42:39 +0100

    added domain variable now working good with wordpress install

--- srcs/requirements/wordpress/conf/wordpress_setup.sh
+++ srcs/requirements/wordpress/conf/wordpress_setup.sh
@@ -27,7 +27,7 @@ then
     wp core download --path=/var/www/html --allow-root #telecharge les fichiers d'installations de WP dans mon dossier.
     cd /var/www/html
     wp config create --dbhost=mariadb --dbname=$WP_DATABASE --dbuser=$WP_USER --dbpass=$WP_PASSWORD --allow-root #Connection a la Database MYSQL
-    wp core install --url=localhost --title="testinception" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root #Installation de wordpress
+    wp core install --url=https://$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL --allow-root #Installation de wordpress
     wp user create $WP_USER2_NAME  $WP_USER2_MAIL --user_pass=$WP_USER2_PASS --allow-root #Creation d'un deuxieme user pour le sujet.
 else
     echo "[INFO] Wordpress is already installed"
